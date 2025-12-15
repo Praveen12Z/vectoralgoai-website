210880632441d0e1c544ae59da5ad6a4463ca1b2
Praveen12Z <64327771+Praveen12Z@users.noreply.github.com>
2025-12-15 15:59:33 +0100

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+
 
 export const metadata: Metadata = {
   title: "VectorAlgoAI",
